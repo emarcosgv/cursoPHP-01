1c1fb90902c8bd43164785910d7adf019fe9d0dc
Emmanuel Marcos Gabriel Villalba <evillalba@frba.utn.edu.ar>
2022-09-09 16:33:49 -0300

    Initial commit

---
+++ paraPrueba.php
@@ -0,0 +1,10 @@
+<?php
+$unNumero = 10;
+$otroNumero = 15;
+
+if ($unNumero > 1) {
+    echo "estoy en el camino del True\n";
+    echo "soy la segunda linea del True\n";
+}
+echo "soy otra linea.";
+
